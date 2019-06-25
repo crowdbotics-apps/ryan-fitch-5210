429fc42830df77c66b1f03bd8cb9919a59d16982
Crowdbotics <team@crowdbotics.com>
2019-06-25 23:28:55 +0000

    initial commit

---
+++ backend/ryan_fitch_5210/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ryan_fitch_5210 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ryan_fitch_5210.settings')
+
+application = get_wsgi_application()
